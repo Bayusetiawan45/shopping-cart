b2f2d3084bf750a485e24d6f6f8cef017b5e4351
bayu <bayusetiawanipb@gmail.com>
2022-10-17 17:03:35 +0700

    formatting code

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -11,8 +10,8 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Bayu')
+      'process.env.name': JSON.stringify('Bayu'),
     }),
     new ReactRefreshWebpackPlugin(),
-  ]
+  ],
 }
